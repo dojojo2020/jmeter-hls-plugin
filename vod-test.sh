60ae4eacbb91c4933680fe1a9ed462a1cc9c05eb
dojojo2020 <56931399+dojojo2020@users.noreply.github.com>
2022-07-06 23:57:26 -0700

    Update vod-test.sh

--- vod-test.sh
+++ vod-test.sh
@@ -5,9 +5,9 @@
 # Example for using User Defined Variables with JMeter
 # These will be substituted in JMX test script
 # See also: http://stackoverflow.com/questions/14317715/jmeter-changing-user-defined-variables-from-command-line
-export TARGET_HOST="vod.k3s-hhii.amp"
+export TARGET_HOST="vod.k8s.example"
 export TARGET_PORT="80"
-export TARGET_PATH="/hls/Ampere_AI_,240p.mp4,360p.mp4,480p.mp4,720p.mp4,1080p.mp4,.en_US.vtt,.urlset/master.m3u8"
+export TARGET_PATH="/hls/DEMO_,240p.mp4,360p.mp4,480p.mp4,720p.mp4,1080p.mp4,.en_US.vtt,.urlset/master.m3u8"
 export TARGET_KEYWORD="vod"
 
 T_DIR=vod-tests
