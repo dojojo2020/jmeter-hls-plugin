96443efc4651093993f93f059b50225bff77a949
dojojo2020 <56931399+dojojo2020@users.noreply.github.com>
2022-09-03 11:19:43 -0700

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Inspired from https://github.com/hhcordero/docker-jmeter-client and
+# Based on https://github.com/justb4/docker-jmeter/blob/master/entrypoint.sh
+# Basically runs jmeter, assuming the PATH is set to point to JMeter bin-dir (see Dockerfile)
+#
+# This script expects the standdard JMeter command parameters.
+#
+
+# Install jmeter plugins available on /plugins volume
+if [ -d $JMETER_CUSTOM_PLUGINS_FOLDER ]
+then
+    for plugin in ${JMETER_CUSTOM_PLUGINS_FOLDER}/*.jar; do
+        cp $plugin ${JMETER_HOME}/lib/ext
+    done;
+fi
+
+# Execute JMeter command
+set -e
+freeMem=`awk '/MemAvailable/ { print int($2/1024) }' /proc/meminfo`
+
+[[ -z ${JVM_XMN} ]] && JVM_XMN=$(($freeMem/10*2))
+[[ -z ${JVM_XMS} ]] && JVM_XMS=$(($freeMem/10*8))
+[[ -z ${JVM_XMX} ]] && JVM_XMX=$(($freeMem/10*8))
+
+export JVM_ARGS="-Xmn${JVM_XMN}m -Xms${JVM_XMS}m -Xmx${JVM_XMX}m"
+
+echo "START Running Jmeter on `date`"
+
+echo "JVM_ARGS=${JVM_ARGS}"
+echo "jmeter args=$@"
+
+# Keep entrypoint simple: we must pass the standard JMeter arguments
+EXTRA_ARGS=-Dlog4j2.formatMsgNoLookups=true
+echo "jmeter ALL ARGS=${EXTRA_ARGS} $@"
+jmeter ${EXTRA_ARGS} $@
+
+echo "END Running Jmeter on `date`"
