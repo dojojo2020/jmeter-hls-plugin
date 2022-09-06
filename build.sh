e4f3f2a6155a362841c769d3218829f793be1358
dojojo2020 <56931399+dojojo2020@users.noreply.github.com>
2022-09-05 22:01:28 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,13 @@
 IMAGE_TIMEZONE="North America/Pacific Time" 
 JMETER_VERSION="5.4.3"   
 
-# Example build line
+echo "Building mrdojojo/jmeter-hls-plugin:${JMETER_VERSION}"
+
+#Build the jmeter-hls-plugin container image
 podman  build  --build-arg JMETER_VERSION=${JMETER_VERSION} --build-arg TZ="${IMAGE_TIMEZONE}" -t "mrdojojo/jmeter-hls-plugin:${JMETER_VERSION}" .
+
+echo "Building mrdojojo/jmeter-hls-server:${JMETER_VERSION}"
+
+#Build the jmeter-hls-server container image
+cd server-mode
+podman  build  --build-arg JMETER_VERSION=${JMETER_VERSION} --build-arg TZ="${IMAGE_TIMEZONE}" -t "mrdojojo/jmeter-hls-server:${JMETER_VERSION}" .
