27aaa1fc8d5beaab9172ef6a08609acc95469658
dojojo2020 <56931399+dojojo2020@users.noreply.github.com>
2022-09-05 22:19:05 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@ JMETER_VERSION="5.4.3"
 echo "Building mrdojojo/jmeter-hls-plugin:${JMETER_VERSION}"
 
 #Build the jmeter-hls-plugin container image
-podman  build  --build-arg JMETER_VERSION=${JMETER_VERSION} --build-arg TZ="${IMAGE_TIMEZONE}" -t "mrdojojo/jmeter-hls-plugin:${JMETER_VERSION}" .
+podman  build --no-cache --build-arg JMETER_VERSION=${JMETER_VERSION} --build-arg TZ="${IMAGE_TIMEZONE}" -t "mrdojojo/jmeter-hls-plugin:${JMETER_VERSION}" .
 
 echo "Building mrdojojo/jmeter-hls-server:${JMETER_VERSION}"
 
 #Build the jmeter-hls-server container image
 cd server-mode
-podman  build  --build-arg JMETER_VERSION=${JMETER_VERSION} --build-arg TZ="${IMAGE_TIMEZONE}" -t "mrdojojo/jmeter-hls-server:${JMETER_VERSION}" .
+podman  build --no-cache --build-arg JMETER_VERSION=${JMETER_VERSION} --build-arg TZ="${IMAGE_TIMEZONE}" -t "mrdojojo/jmeter-hls-server:${JMETER_VERSION}" .
